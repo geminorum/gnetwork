d12b59e406ce110501719f27d19095cf7b60197c
Nasser Rafie <contact@geminorum.ir>
2015-05-19 20:09:58 +0430

    0.2.0 - modules / pluggable

---
+++ includes/pluggable.php
@@ -0,0 +1,18 @@
+<?php defined( 'ABSPATH' ) or die( 'Restricted access' );
+
+// Allow WordPress to use bcrypt for passwords.
+// https://gist.github.com/chrisguitarguy/4122483
+if( ! function_exists( 'wp_hash_password' ) ):
+	function wp_hash_password( $password ) {
+		global $wp_hasher;
+
+		if( empty( $wp_hasher ) ){
+			require_once( ABSPATH.'wp-includes/class-phpass.php' );
+
+			// second arg: make password non-portable (eg. allow bcrypt)
+			$wp_hasher = new PasswordHash( 16, false );
+		}
+
+		return $wp_hasher->HashPassword( $password );
+	}
+endif;
