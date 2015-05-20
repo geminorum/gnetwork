9e3f6ffd4b8fd7b6dacea5398514cff9dabfcf52
Nasser Rafie <contact@geminorum.ir>
2015-05-20 07:57:33 +0430

    version bump

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+### 0.2.1
+* languages: include mo files
+* themes: css font include moved above
+
+### 0.2.0
+* github publish
--- gnetwork.php
+++ gnetwork.php
@@ -3,7 +3,7 @@
 Plugin Name: gNetwork
 Plugin URI: http://gmeinorum.ir/wordpress/gnetwork
 Description: Network Helper
-Version: 0.2.0
+Version: 0.2.1
 Author: geminorum
 Author URI: http://geminorum.ir/
 Network: true
@@ -14,7 +14,7 @@ GitHub Plugin URI: https://github.com/geminorum/gnetwork
 GitHub Branch: develop
 */
 
-define( 'GNETWORK_VERSION', '0.2.0' );
+define( 'GNETWORK_VERSION', '0.2.1' );
 define( 'GNETWORK_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GNETWORK_URL', plugin_dir_url( __FILE__ ) );
 
