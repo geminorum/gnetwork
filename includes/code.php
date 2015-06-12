6756fa36247edea59d61d638d4889e7c11d66497
Nasser Rafie <contact@geminorum.ir>
2015-06-12 09:04:09 +0430

    gist shortcode
    * new gist embed shortcode: `[github-gist]` using
    [gist-embed](https://github.com/blairvanderhoof/gist-embed)
    *
    `[github-readme]` now grabs wiki pages and any markdown on the repo as
    well
    * add `CONTRIBUTING.md` to the list of relative links
    * cleanup

--- includes/code.php
+++ includes/code.php
@@ -3,45 +3,65 @@
 class gNetworkCode extends gNetworkModuleCore
 {
 
-	var $_network    = false;
-	var $_option_key = false;
+	var $_network    = FALSE;
+	var $_option_key = FALSE;
 
 	public function setup_actions()
 	{
-		add_action( 'init', array( & $this, 'init' ), 12 );
+		add_action( 'init', array( &$this, 'init' ), 12 );
+
 	}
 
 	public function init()
 	{
 		$this->shortcodes( array(
-			'github' => 'shortcode_github',
+			'github'        => 'shortcode_github',
 			'github-readme' => 'shortcode_github_readme',
+			'github-gist'   => 'shortcode_github_gist',
 		) );
+
+		// NOT WORKING: gist id is now diffrent from this pattern
+		// FIXME: add option to enable this
+		// add_filter( 'the_content', array( &$this, 'the_content_gist_shortcode' ), 9 );
 	}
 
-	// Originally based on : GitHub README v0.0.3
+	// Originally based on : GitHub README v0.1.0
 	// by Jason Stallings : http://jason.stallin.gs
 	// https://github.com/octalmage/github-readme
 	// https://wordpress.org/plugins/github-readme/
-	public function shortcode_github_readme( $atts, $content = null, $tag = '' )
+	public function shortcode_github_readme( $atts, $content = NULL, $tag = '' )
 	{
 		$args = shortcode_atts( array(
-			'repo' => 'geminorum/gnetwork',
-			'trim' => 0,
+			'repo'    => 'geminorum/gnetwork',
+			'trim'    => 0,
+			'type'    => 'readme', // 'readme', 'markdown', 'wiki'
+			'file'    => '/readme', // markdown page
+			'branch'  => 'master', // markdown branch
+			'page'    => '', // wiki page
+			'context' => NULL,
 		), $atts, $tag );
+
+		if ( FALSE === $args['context'] )
+			return NULL;
 
 		$html = $content;
 		$this->github_repo = $args['repo'];
 
-		$key = 'gnetwork_code_githubreadme_'.$args['repo'].'_'.$args['trim'];
-
+		$key = 'gnetwork_code_githubreadme_'.$args['repo'].'_'.$args['type'].'_'.$args['trim'];
 
 		if ( gNetworkUtilities::isFlush() )
 			delete_site_transient( $key );
 
-		if ( false === ( $html = get_site_transient( $key ) ) ) {
+		if ( FALSE === ( $html = get_site_transient( $key ) ) ) {
 
-			if ( $json = self::getJSON( 'https://api.github.com/repos/'.$args['repo'].'/readme' ) ) {
+			switch ( $args['type'] ) {
+				default :
+				case 'readme'   : $url = 'https://api.github.com/repos/'.$args['repo'].'/readme'; break;
+				case 'markdown' : $url = 'https://raw.githubusercontent.com/'.$args['repo'].'/'.$args['branch'].'/'.$args['file']; break;
+				case 'wiki'     : $url = 'https://raw.githubusercontent.com/wiki/'.$args['repo'].'/'.$args['page'].'.md'; break;
+			}
+
+			if ( $json = self::getJSON( $url ) ) {
 
 				$md = base64_decode( $json->content );
 				if ( $args['trim'] )
@@ -49,14 +69,15 @@ class gNetworkCode extends gNetworkModuleCore
 
 				// TODO: must use : http://parsedown.org/demo
 
+				// if ( ! class_exists( 'Markdown' ) )
+				// 	require_once( GNETWORK_DIR.'assets/libs/php-markdown/Michelf/Markdown.inc.php' );
+
 				if ( ! class_exists( 'MarkdownExtra' ) )
-				//if ( ! class_exists( 'Markdown' ) )
-					//require_once( GNETWORK_DIR.'assets/libs/php-markdown/Michelf/Markdown.inc.php' );
 					require_once( GNETWORK_DIR.'assets/libs/php-markdown/Michelf/MarkdownExtra.inc.php' );
 
-				//defined( 'GPERSIANDATE_SKIP' ) or define( 'GPERSIANDATE_SKIP', true );
+				// __gpersiandate_skip();
 
-				//$html = \Michelf\Markdown::defaultTransform( $md );
+				// $html = \Michelf\Markdown::defaultTransform( $md );
 				$html = \Michelf\MarkdownExtra::defaultTransform( $md );
 
 				// http://www.the-art-of-web.com/php/parse-links/
@@ -70,16 +91,19 @@ class gNetworkCode extends gNetworkModuleCore
 		if ( gNetworkUtilities::isDev() )
 			delete_site_transient( $key );
 
-		return '<div class="gnetwork-wrap gnetwork-github-readme" data-github-repo="'.$args['repo'].'">'.$html.'</div>';
+		return '<div class="gnetwork-wrap-shortcode github-readme" data-github-repo="'.$args['repo'].'">'.$html.'</div>';
 	}
 
 	public function github_readme_link_cb( $matchs )
 	{
-		if ( in_array( strtolower( $matchs['2'] ), array(
+		$files =  array(
+			'contributing.md',
 			'changes.md',
 			'readme.md',
 			'readme.txt',
-		) ) )
+		);
+
+		if ( in_array( strtolower( $matchs['2'] ), $files ) )
 			return '<a href="https://github.com/'.$this->github_repo.'/blob/master/'.$matchs[2].'">'.$matchs[3].'</a>';
 
 		return $matchs[0];
@@ -89,13 +113,84 @@ class gNetworkCode extends gNetworkModuleCore
 	// by Jason Stallings
 	// http://json.sx/projects/github-shortcode/
 	// https://wordpress.org/plugins/github-shortcode/
-	public function shortcode_github( $atts, $content = null, $tag = '' )
+	public function shortcode_github( $atts, $content = NULL, $tag = '' )
 	{
 		$args = shortcode_atts( array(
-			'repo' => 'geminorum/gnetwork',
+			'repo'    => 'geminorum/gnetwork',
+			'context' => NULL,
 		), $atts, $tag );
 
-		wp_enqueue_script( 'gnetwork-github-repowidget', GNETWORK_URL.'assets/js/jquery.github-repowidget.min.js', array( 'jquery' ), '20150130', true );
-		return "<div class=\"github-widget\" data-repo=\"".$args['repo']."\"></div>";
+		if ( FALSE === $args['context'] )
+			return NULL;
+
+		wp_enqueue_script( 'gnetwork-code-githubrepowidget', GNETWORK_URL.'assets/js/jquery.github-repowidget.min.js', array( 'jquery' ), '20150130', TRUE );
+
+		return '<div class="gnetwork-wrap-shortcode github" data-github-repo="'.$args['repo'].'"></div>';
+	}
+
+	// https://github.com/blairvanderhoof/gist-embed
+	// http://blairvanderhoof.com/gist-embed/
+	public function shortcode_github_gist( $atts, $content = NULL, $tag = '' )
+	{
+		$args = shortcode_atts( array(
+			'id'                => FALSE,
+			'hide-line-numbers' => FALSE,
+			'hide-footer'       => TRUE,
+			'file'              => FALSE,
+			'line'              => FALSE, // EXAMPLES: '2', '2-4', '1,3-4', '2,3,4'
+			'highlight'         => FALSE, // EXAMPLES: '2', '2-4', '1,3-4', '2,3,4'
+			'loading'           => FALSE,
+			'context'           => NULL,
+		), $atts, $tag );
+
+		if ( FALSE === $args['context'] )
+			return NULL;
+
+		if ( FALSE == $args['id'] )
+			return $content;
+
+		$html = gNetworkUtilities::html( 'code', array(
+			'data-gist-id'                => $args['id'],
+			'data-gist-hide-line-numbers' => $args['hide-line-numbers'] ? 'true' : FALSE,
+			'data-gist-hide-footer'       => $args['hide-footer'] ? 'true' : FALSE,
+			'data-gist-file'              => $args['file'] ? $args['file'] : FALSE,
+			'data-gist-line'              => $args['line'] ? $args['line'] : FALSE,
+			'data-gist-highlight-line'    => $args['highlight'] ? $args['highlight'] : FALSE,
+			'data-gist-show-loading'      => $args['loading'] ? FALSE : 'false',
+		), NULL );
+
+		wp_enqueue_script( 'gnetwork-code-gistembed', GNETWORK_URL.'assets/js/jquery.gist-embed.min.js', array( 'jquery' ), '2.1', TRUE );
+
+		return '<div class="gnetwork-wrap-shortcode github-gist" data-github-gist="'.$args['id'].'">'.$html.'</div>';
+	}
+
+	// ORIGINAL METHOD
+	// https://css-tricks.com/snippets/wordpress/detect-gists-and-embed-them/
+	public function shortcode_github_gist_OLD( $atts, $content = NULL, $tag = '' )
+	{
+		$args = shortcode_atts( array(
+			'id'      => FALSE,
+			'file'    => FALSE,
+			'context' => NULL,
+		), $atts, $tag );
+
+		if ( FALSE === $args['context'] )
+			return NULL;
+
+		if ( FALSE == $args['id'] )
+			return $content;
+
+		$html = sprintf( '<script src="https://gist.github.com/%s.js%s"></script>',
+			$args['id'],
+			$args['file'] ? '?file='.$args['file'] : ''
+		);
+
+		return '<div class="gnetwork-wrap-shortcode github-gist" data-github-gist="'.$args['id'].'">'.$html.'</div>';
+	}
+
+	// autoreplace gist links to shortcodes
+	public function the_content_gist_shortcode( $content )
+	{
+		return preg_replace( '/https:\/\/gist.github.com\/([\d]+)[\.js\?]*[\#]*file[=|_]+([\w\.]+)(?![^<]*<\/a>)/i', '', $content );
 	}
 }
