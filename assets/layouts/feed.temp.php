db0802183a9af25f1be485eaf2157dc7de45e86a
Nasser Rafie <contact@geminorum.ir>
2015-09-18 23:36:30 +0430

    :wrench:

--- assets/layouts/feed.temp.php
+++ assets/layouts/feed.temp.php
@@ -2,13 +2,13 @@
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'."\n"; ?>
 <rss version="2.0">
 	<channel>
-		<title><?php bloginfo_rss('name'); wp_title_rss(); ?></title>
-		<link><?php bloginfo_rss('url'); ?></link>
+		<title><?php bloginfo_rss( 'name' ); wp_title_rss(); ?></title>
+		<link><?php bloginfo_rss( 'url' ); ?></link>
 		<item>
 			<title><?php echo $title; ?></title>
 			<link><?php echo $link; ?></link>
 			<description><?php echo $desc; ?></description>
-			<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', current_time( 'mysql', true ), false ); ?></pubDate>
+			<pubDate><?php echo mysql2date( 'D, d M Y H:i:s +0000', current_time( 'mysql', TRUE ), FALSE ); ?></pubDate>
 		</item>
 	</channel>
 </rss>
