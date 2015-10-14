1d91e3e682b2a863689eaa927c80155b19e834a4
Nasser Rafie <contact@geminorum.ir>
2015-10-14 23:01:44 +0330

    github readme as network overview

--- admin/network.overview.php
+++ admin/network.overview.php
@@ -1,1 +1,9 @@
 <?php defined( 'ABSPATH' ) or die( 'Restricted access' );
+
+global $gNetwork;
+
+if ( isset( $gNetwork->code ) ) {
+	echo $gNetwork->code->shortcode_github_readme( array(
+		'repo' => 'geminorum/gnetwork',
+	) );
+}
