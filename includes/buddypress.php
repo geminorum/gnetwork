e9f1ac573c825c040ed65f14faef2973a65a6c19
Nasser Rafie <contact@geminorum.ir>
2015-11-13 02:42:49 +0330

    disable cover images

--- includes/buddypress.php
+++ includes/buddypress.php
@@ -29,6 +29,12 @@ class gNetworkBuddyPress extends gNetworkModuleCore
 
 		add_action( 'bp_ajax_querystring', array( $this, 'bp_ajax_querystring' ), 20, 2 );
 
+		if ( ! $this->options['cover_images'] ) {
+			// @SOURCE: http://buddydev.com/buddypress/disable-cover-images-for-buddypress-2-4/
+			add_filter( 'bp_is_profile_cover_image_active', '__return_false' );
+			add_filter( 'bp_is_groups_cover_image_active', '__return_false' );
+		}
+
 		if ( $this->options['complete_signup'] )
 			add_action( 'bp_complete_signup', array( $this, 'bp_complete_signup' ) );
 
@@ -57,6 +63,7 @@ class gNetworkBuddyPress extends gNetworkModuleCore
 	public function default_options()
 	{
 		return array(
+			'cover_images'    => '1',
 			'complete_signup' => '',
 
 			'tos_display' => 0,
@@ -86,6 +93,13 @@ class gNetworkBuddyPress extends gNetworkModuleCore
 					'title'       => _x( 'Complete Signup', 'BuddyPress Module', GNETWORK_TEXTDOMAIN ),
 					'desc'        => _x( 'Redirect users after successful registration.', 'BuddyPress Module', GNETWORK_TEXTDOMAIN ),
 					'field_class' => 'large-text',
+				),
+				array(
+					'field'   => 'cover_images',
+					'type'    => 'enabled',
+					'title'   => _x( 'Cover Images', 'BuddyPress Module', GNETWORK_TEXTDOMAIN ),
+					'desc'    => _x( 'Disable the cover image completely', 'BuddyPress Module', GNETWORK_TEXTDOMAIN ),
+					'default' => '1',
 				),
 			),
 		);
