c9a91838a486e3f199f5305685ddbe7e4198f8db
Nasser Rafie <contact@geminorum.ir>
2016-04-05 17:07:00 +0430

    shortcode term title helper

--- includes/modulecore.php
+++ includes/modulecore.php
@@ -1024,6 +1024,42 @@ class gNetworkModuleCore extends gNetworkBaseCore
 		return self::html( $block ? 'div' : 'span', array( 'class' => $classes ), $html );
 	}
 
+	public static function shortcodeTermTitle( $atts, $term = FALSE )
+	{
+		$args = self::atts( array(
+			'title'        => NULL, // FALSE to disable
+			'title_link'   => NULL, // FALSE to disable
+			'title_title'  => '',
+			'title_tag'    => 'h3',
+			'title_anchor' => 'term-',
+		), $atts );
+
+		if ( is_null( $args['title'] ) )
+			$args['title'] = $term ? sanitize_term_field( 'name', $term->name, $term->term_id, $term->taxonomy, 'display' ) : FALSE;
+
+		if ( $args['title'] ) {
+			if ( is_null( $args['title_link'] ) && $term )
+				$args['title'] = self::html( 'a', array(
+					'href'  => get_term_link( $term, $term->taxonomy ),
+					'title' => $args['title_title'],
+				), $args['title'] );
+
+			else if ( $args['title_link'] )
+				$args['title'] = self::html( 'a', array(
+					'href'  => $args['title_link'],
+					'title' => $args['title_title'],
+				), $args['title'] );
+		}
+
+		if ( $args['title'] && $args['title_tag'] )
+			$args['title'] = self::html( $args['title_tag'], array(
+				'id'    => $term ? $args['title_anchor'].$term->term_id : FALSE,
+				'class' => '-title',
+			), $args['title'] );
+
+		return $args['title'];
+	}
+
 	// HELPER
 	public static function counted( $message = NULL, $count = NULL, $class = 'updated' )
 	{
