82b41b15ef8bd5c34256fa0d0b1722b92d5ec186
Nasser Rafie <contact@geminorum.ir>
2016-05-06 13:17:42 +0430

    more mb helpers

--- includes/basecore.php
+++ includes/basecore.php
@@ -1559,4 +1559,28 @@ class BaseCore
 	{
 		return ( ( $thing instanceof \WP_Error ) || ( $thing instanceof Error ) );
 	}
+
+	// @SEE: `mb_convert_case()`
+	public static function strToLower( $string, $encoding = 'UTF-8' )
+	{
+		return function_exists( 'mb_strtolower' ) ? mb_strtolower( $string, $encoding ) : strtolower( $string );
+	}
+
+	public static function strLen( $string, $encoding = 'UTF-8' )
+	{
+		return function_exists( 'mb_strlen' ) ? mb_strlen( $string, $encoding ) : strlen( $string );
+	}
+
+	public static function subStr( $string, $start = 0, $length = 1, $encoding = 'UTF-8' )
+	{
+		return function_exists( 'mb_substr' ) ? mb_substr( $string, $start, $length, $encoding ) : substr( $string, $start, $length );
+	}
+
+	public static function internalEncoding( $encoding = 'UTF-8' )
+	{
+		if ( function_exists( 'mb_internal_encoding' ) )
+			return mb_internal_encoding( $encoding );
+
+		return FALSE;
+	}
 }
