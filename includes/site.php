25f48fac2733113593c3abc8b462de0712a3d46f
Nasser Rafie <contact@geminorum.ir>
2016-05-30 15:48:30 +0430

    redirect to page if registration disabled
    + string contexts updated

--- includes/site.php
+++ includes/site.php
@@ -7,6 +7,12 @@ class Site extends ModuleCore
 
 	protected $key = 'general';
 
+	protected function setup_actions()
+	{
+		if ( $this->options['page_signup'] && ! is_admin() )
+			add_action( 'before_signup_header', array( $this, 'before_signup_header' ), 1 );
+	}
+
 	public function setup_menu( $context )
 	{
 		Network::registerMenu( $this->key,
@@ -19,11 +25,17 @@ class Site extends ModuleCore
 	{
 		return array(
 			'admin_locale' => 'en_US',
+			'page_signup'  => '0',
 		);
 	}
 
 	public function default_settings()
 	{
+		$exclude = array_filter( array(
+			get_option( 'page_on_front' ),
+			get_option( 'page_for_posts' ),
+		) );
+
 		$settings = array();
 
 		if ( class_exists( __NAMESPACE__.'\\Locale' ) ) {
@@ -31,14 +43,32 @@ class Site extends ModuleCore
 				array(
 					'field'       => 'admin_locale',
 					'type'        => 'select',
-					'title'       => _x( 'Network Language', 'Site Module', GNETWORK_TEXTDOMAIN ),
-					'description' => _x( 'Despite of the site language, always display network admin in this locale', 'Site Module', GNETWORK_TEXTDOMAIN ),
+					'title'       => _x( 'Network Language', 'Site Module: Setting Title', GNETWORK_TEXTDOMAIN ),
+					'description' => _x( 'Despite of the site language, always display network admin in this locale', 'Site Module: Setting Description', GNETWORK_TEXTDOMAIN ),
 					'default'     => 'en_US',
 					'values'      => Utilities::sameKey( Locale::available() ),
 				),
 			);
 		}
 
+		$settings['_signup'] = array(
+			array(
+				'field'       => 'page_signup',
+				'type'        => 'page',
+				'title'       => _x( 'Page for Signup', 'Site Module: Setting Title', GNETWORK_TEXTDOMAIN ),
+				'description' => _x( 'Redirects signups into this page, if registration disabled', 'Site Module: Setting Description', GNETWORK_TEXTDOMAIN ),
+				'default'     => '0',
+				'exclude'     => $exclude,
+				'after'       => self::settingsFieldAfterIcon( self::getNewPostTypeLink( 'page' ) ),
+			),
+		);
+
 		return $settings;
+	}
+
+	public function before_signup_header()
+	{
+		if ( 'none' == get_site_option( 'registration', 'none' ) )
+			self::redirect( get_page_link( $this->options['page_signup'] ) );
 	}
 }
