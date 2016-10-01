e881ddb67294ad535895113359196e7cbe27930a
Nasser Rafie <contact@geminorum.ir>
2016-10-01 12:29:28 +0330

    core/date: :new:  class

---
+++ includes/core/date.php
@@ -0,0 +1,26 @@
+<?php namespace geminorum\gNetwork;
+
+defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+class Date extends Base
+{
+
+	public static function monthFirstAndLast( $year, $month, $format = 'Y-m-d H:i:s' )
+	{
+		$start = new DateTime( $year.'-'.$month.'-01 00:00:00' );
+		$end   = $start->modify( '+1 month -1 day -1 minute' );
+
+		return array(
+			$start->format( $format ),
+			$end->format( $format ),
+		);
+	}
+
+	public static function htmlDateTime( $time, $format = 'l, F j, Y', $title = FALSE )
+	{
+		return HTML::tag( 'time', array(
+			'datetime' => date( 'c', $time ),
+			'title'    => $title,
+		), date_i18n( $format, $time ) );
+	}
+}
