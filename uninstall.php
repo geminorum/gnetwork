79206d2677e51934c9612a4d274749655535d69c
Nasser Rafie <contact@geminorum.ir>
2017-04-02 22:05:13 +0430

    plugin/uninstall: option deletion for site & blog

---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
+
+// ensure this plugin is actually being uninstalled
+defined( 'WP_UNINSTALL_PLUGIN' ) or exit();
+
+delete_option( 'gnetwork_blog' );
+delete_site_option( 'gnetwork_site' );
