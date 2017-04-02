7120d983a8aa738916227e5f0c732d9203293de2
Nasser Rafie <contact@geminorum.ir>
2017-04-02 21:56:48 +0430

    main/constants: prevent extra redirect by trailing slash

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,7 @@
 $gnetwork_constants = array(
 
 	'GNETWORK_TEXTDOMAIN'            => 'gnetwork',
-	'GNETWORK_BASE'                  => network_home_url(),
+	'GNETWORK_BASE'                  => network_home_url( '/' ),
 	'GNETWORK_NAME'                  => ( is_multisite() ? get_site_option( 'site_name' ) : get_option( 'blogname' ) ),
 	'GNETWORK_LOGO'                  => 'login.png', // default logo image file, must be on wp-content
 	// 'GNETWORK_WPLANG'                => 'en_US', // define if necessary
