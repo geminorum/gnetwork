8edadd30d44b7bb4d61f99884283585d86a75662
Nasser Rafie <contact@geminorum.ir>
2017-04-07 00:24:26 +0430

    main/pluggable: again include phpass

--- includes/pluggable.php
+++ includes/pluggable.php
@@ -6,9 +6,13 @@ if ( ! function_exists( 'wp_hash_password' ) ):
 	function wp_hash_password( $password ) {
 		global $wp_hasher;
 
-		if ( empty( $wp_hasher ) )
+		if ( empty( $wp_hasher ) ) {
+
+			require_once( ABSPATH.WPINC.'/class-phpass.php' );
+
 			// second arg: make password non-portable (eg. allow bcrypt)
 			$wp_hasher = new \PasswordHash( 16, FALSE );
+		}
 
 		return $wp_hasher->HashPassword( trim( $password ) );
 	}
