b1051f1ecc7285339133aad0c2849013fb96cdcc
Nasser Rafie <contact@geminorum.ir>
2017-06-14 03:52:38 +0430

    main/plugin: better file loader

--- includes/plugin.php
+++ includes/plugin.php
@@ -58,11 +58,15 @@ class Plugin
 		add_filter( 'mce_external_languages',[ $this, 'mce_external_languages' ] );
 	}
 
-	private function files( $stack )
+	private function files( $stack, $check = TRUE, $base = GNETWORK_DIR )
 	{
 		foreach ( (array) $stack as $path )
-			if ( file_exists( GNETWORK_DIR.'includes/'.$path.'.php' ) )
-				require_once( GNETWORK_DIR.'includes/'.$path.'.php' );
+
+			if ( ! $check )
+				require_once( $base.'includes/'.$path.'.php' );
+
+			else if ( file_exists( $base.'includes/'.$path.'.php' ) )
+				require_once( $base.'includes/'.$path.'.php' );
 	}
 
 	private function require_core()
