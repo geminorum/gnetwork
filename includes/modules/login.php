654cb02f344bfc394cd4a21f64b8a70838598735
Nasser Rafie <contact@geminorum.ir>
2017-08-03 22:16:11 +0430

    module/login: blog name before logs

--- includes/modules/login.php
+++ includes/modules/login.php
@@ -230,16 +230,16 @@ class Login extends gNetwork\Module
 
 		if ( FALSE === $correct ) {
 
-			Logger::ALERT( 'LOGIN-MATH: not properly configured'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
+			Logger::ALERT( 'LOGIN-MATH: '.WordPress::currentBlog().': not properly configured'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
 
 			wp_die( _x( '<strong>This site is not properly configured.</strong> Please ask this site\'s web developer to review for information on how to resolve this issue.', 'Modules: Login', GNETWORK_TEXTDOMAIN ) );
 
 		} else if ( FALSE === $salted || $salted != $correct ) {
 
 			if ( FALSE === $salted )
-				Logger::WARNING( 'LOGIN-MATH: not posting answer'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
+				Logger::WARNING( 'LOGIN-MATH: '.WordPress::currentBlog().': not posting answer'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
 			else
-				Logger::NOTICE( 'LOGIN-MATH: failed to correctly answer'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
+				Logger::NOTICE( 'LOGIN-MATH: '.WordPress::currentBlog().': failed to correctly answer'.sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
 
 			wp_die( _x( '<strong>You failed to correctly answer the math problem.</strong> This is used to combat spam Please use your browser\'s back button to return to the login form, press the "refresh" button to generate a new math problem, and try to log in again.', 'Modules: Login', GNETWORK_TEXTDOMAIN ) );
 		}
@@ -250,7 +250,7 @@ class Login extends gNetwork\Module
 	// TODO: add logger to logout / has no action hook with user info!
 	public function wp_login( $user_login, $user )
 	{
-		Logger::NOTICE( sprintf( 'LOGGED-IN: %s', $user_login ) );
+		Logger::NOTICE( 'LOGGED-IN: '.WordPress::currentBlog().': '.sprintf( '%s', $user_login ) );
 	}
 
 	public function wp_login_errors( $errors, $redirect_to )
@@ -272,7 +272,7 @@ class Login extends gNetwork\Module
 
 		foreach ( $errors->get_error_codes() as $error )
 			if ( in_array( $error, $log ) )
-				Logger::NOTICE( 'LOGIN-ERRORS: '.str_replace( '_', ' ', $error ).sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
+				Logger::NOTICE( 'LOGIN-ERRORS: '.WordPress::currentBlog().': '.str_replace( '_', ' ', $error ).sprintf( ': %s', self::req( 'log', '(EMPTY)' ) ) );
 
 		return $errors;
 	}
