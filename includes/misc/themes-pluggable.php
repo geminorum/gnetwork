b0b5ebddc96ac8b1458bd076969587634bfe1233
Nasser Rafie <contact@geminorum.ir>
2018-04-14 18:36:56 +0430

    :wrench: removed misc

--- includes/misc/themes-pluggable.php
+++
@@ -1,5 +0,0 @@
-<?php defined( 'ABSPATH' ) or die( header( 'HTTP/1.0 403 Forbidden' ) );
-
-if ( ! function_exists( 'untitled_posted_on' ) ) : function untitled_posted_on() {
-	\geminorum\gNetwork\Modules\Themes::postedOn();
-} endif;
