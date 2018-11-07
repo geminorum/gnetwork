c2fb860aec5db9fcf40bcff3d5f2c0d7b99fc8f0
Nasser Rafie <contact@geminorum.ir>
2018-11-08 02:06:52 +0330

    :peach:

--- includes/Core/WordPress.php
+++ includes/Core/WordPress.php
@@ -442,19 +442,26 @@ class WordPress extends Base
 		), $extra ), get_bloginfo( 'url' ) );
 	}
 
-	public static function getPostNewLink( $post_type, $extra = array() )
+	public static function getPostNewLink( $posttype, $extra = array() )
 	{
-		return add_query_arg( array_merge( array(
-			'post_type' => $post_type,
-		), $extra ), admin_url( 'post-new.php' ) );
+		$args = 'post' == $posttype ? array() : array( 'post_type' => $posttype );
+
+		return add_query_arg( array_merge( $args, $extra ), admin_url( 'post-new.php' ) );
 	}
 
-	public static function getUserEditLink( $user_id, $extra = array() )
+	public static function getUserEditLink( $user_id, $extra = array(), $network = FALSE, $check = TRUE )
 	{
-		if ( current_user_can( 'edit_user', $user_id ) )
-			return add_query_arg( array_merge( array(
-				'user_id' => $user_id,
-			), $extra ), admin_url( 'user-edit.php' ) );
+		if ( ! $user_id )
+			return FALSE;
+
+		if ( $check && ! current_user_can( 'edit_user', $user_id ) )
+			return FALSE;
+
+		return add_query_arg( array_merge( array(
+			'user_id' => $user_id,
+		), $extra ), $network
+			? network_admin_url( 'user-edit.php' )
+			: admin_url( 'user-edit.php' ) );
 
 		return FALSE;
 	}
