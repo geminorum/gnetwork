c41e91149bf0d823f2e2ff3c4019d5013a99cb72
Nasser Rafie <contact@geminorum.ir>
2018-12-21 07:06:21 +0330

    misc/meta panel: un-html post-content

--- includes/Misc/DebugMetaPanel.php
+++ includes/Misc/DebugMetaPanel.php
@@ -21,6 +21,17 @@ class DebugMetaPanel extends \Debug_Bar_Panel
 
 			$meta = get_term_custom( get_queried_object_id() );
 
+		} else if ( ! is_admin() && is_singular() ) {
+
+			$post = get_post( get_queried_object_id() );
+			$meta = get_post_meta( $post->ID );
+
+			$post->post_content = HTML::dump( $post->post_content, TRUE, FALSE );
+
+			echo '<div class="-post">';
+				HTML::tableSide( $post );
+			echo '</div>';
+
 		} else if ( defined( 'IS_PROFILE_PAGE' ) && IS_PROFILE_PAGE && current_user_can( 'edit_users' ) ) {
 
 			$meta = get_user_meta( get_current_user_id() );
@@ -36,6 +47,8 @@ class DebugMetaPanel extends \Debug_Bar_Panel
 		} else if ( $post = get_post() ) {
 
 			$meta = get_post_meta( $post->ID );
+
+			$post->post_content = HTML::dump( $post->post_content, TRUE, FALSE );
 
 			echo '<div class="-post">';
 				HTML::tableSide( $post );
