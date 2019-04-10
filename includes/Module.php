cfc6f22e96f001b08476d6435ac4adbe29f3ede4
Nasser Rafie <contact@geminorum.ir>
2019-04-10 13:38:38 +0430

    main/module:  schedule only on cron enabled modules

--- includes/Module.php
+++ includes/Module.php
@@ -147,7 +147,8 @@ class Module extends Core\Base
 		if ( method_exists( $this, 'current_screen' ) )
 			add_action( 'current_screen', [ $this, 'current_screen' ], $this->priority_current_screen );
 
-		if ( method_exists( $this, 'schedule_actions' ) )
+		// only on cron enabled modules
+		if ( $this->cron && method_exists( $this, 'schedule_actions' ) )
 			add_action( 'admin_init', [ $this, 'schedule_actions' ] );
 	}
 
