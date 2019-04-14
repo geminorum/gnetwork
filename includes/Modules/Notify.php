7fa60411c12fb7b0c4b0e86dd909e8ceefe9c875
Nasser Rafie <contact@geminorum.ir>
2019-04-14 18:28:11 +0430

    module/notify: drop wraps around emails on logs

--- includes/Modules/Notify.php
+++ includes/Modules/Notify.php
@@ -240,7 +240,7 @@ class Notify extends gNetwork\Module
 
 	public function send_email_change_email( $true, $user, $userdata )
 	{
-		Logger::INFO( sprintf( 'NOTIFY: email changed user %s from <%s> to <%s>',
+		Logger::INFO( sprintf( 'NOTIFY: email changed user %s from %s to %s',
 			$user['user_nicename'], $user['user_email'], $userdata['user_email'] ) );
 
 		return $this->options['disable_email_change'] ? FALSE: $true;
@@ -248,7 +248,7 @@ class Notify extends gNetwork\Module
 
 	public function send_site_admin_email_change_email( $send, $old_email, $new_email )
 	{
-		Logger::siteINFO( 'NOTIFY', sprintf( 'admin email changed from <%s> to <%s>', $old_email, $new_email ) );
+		Logger::siteINFO( 'NOTIFY', sprintf( 'admin email changed from %s to %s', $old_email, $new_email ) );
 
 		return $this->options['disable_admin_email_change'] ? FALSE: $send;
 	}
