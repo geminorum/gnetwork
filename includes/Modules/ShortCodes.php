2893ccba7e2fa9cd7e079dec2eaae6a4fdf7b093
Nasser Rafie <contact@geminorum.ir>
2023-05-25 01:53:43 +0330

    :up: pdfobject v2.2.11

--- includes/Modules/ShortCodes.php
+++ includes/Modules/ShortCodes.php
@@ -1165,7 +1165,7 @@ class ShortCodes extends gNetwork\Module
 		$selector = $this->selector( 'pdfobject-%2$s' );
 		$this->scripts_nojquery[$selector] = 'PDFObject.embed("'.$args['url'].'", "#'.$selector.'",'.wp_json_encode( $options ).');';
 
-		Scripts::enqueueScriptVendor( 'pdfobject', [], '2.2.10' );
+		Scripts::enqueueScriptVendor( 'pdfobject', [], '2.2.11' );
 		return self::shortcodeWrap( '<div id="'.$selector.'"></div>', 'pdf', $args );
 	}
 
