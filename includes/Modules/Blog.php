1cc611c4f6d3a61321ae9fdce954569d547eec6b
Nasser Rafie <contact@geminorum.ir>
2023-07-14 22:59:30 +0330

    module/blog: remove classic theme styles

--- includes/Modules/Blog.php
+++ includes/Modules/Blog.php
@@ -94,6 +94,7 @@ class Blog extends gNetwork\Module
 					remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
 					remove_action( 'wp_enqueue_scripts', 'wp_common_block_scripts_and_styles' );
 					remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+					remove_action( 'wp_enqueue_scripts', 'wp_enqueue_classic_theme_styles' );
 				}, 99 );
 		}
 
