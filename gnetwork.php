e0353876afeb37f2fa94f5be035c2bbeb1925a53
Nasser Rafie <contact@geminorum.ir>
2025-12-01 14:01:03 +0330

    🔧 change license to GPLv2 or later

--- gnetwork.php
+++ gnetwork.php
@@ -6,7 +6,8 @@ Plugin URI: https://geminorum.ir/wordpress/gnetwork
 Update URI: https://github.com/geminorum/gnetwork
 Description: Network Helper
 Version: 3.18.0
-License: GPLv3+
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Author: geminorum
 Author URI: https://geminorum.ir/
 Network: true
